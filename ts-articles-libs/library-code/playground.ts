63f1806dd4e3e4d3b6043dfa701f6d02eb9ea85a
ladislavlisy <ladislav.lisy@seznam.cz>
2016-04-09 12:35:38 +0200

    initial commit

---
+++ ts-articles-libs/library-code/playground.ts
@@ -0,0 +1,19 @@
+/**
+ * Created by ladislavlisy on 25.03.16.
+ */
+
+export class ConfigArticleCode { 
+
+}
+
+export class  ProcessCategory {
+
+}
+
+export class  Article {
+
+}
+
+export class  Configure {
+
+}
---
+++ ts-articles-libs/test-library-code/playground.test.ts
@@ -0,0 +1,15 @@
+/// <reference path="../typings/main/ambient/mocha/index.d.ts" />
+/// <reference path="../typings/main/ambient/node/index.d.ts" />
+/// <reference path="../typings/main/ambient/should/index.d.ts" />
+
+require("should");
+
+import * as ns from "../library-code/playground";
+
+describe("TestLibrary", () => {
+
+    it("Should_Return_53_For_Articles_Length", () => {
+         true.should.equal(testSymbolOne.isEqualToSymbol(testSymbolTwo));
+    });
+
+});
